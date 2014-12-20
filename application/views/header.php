b2c6cd18b0d97d818c6b9f96cd6895ec750fcb3e
sorav garg <soravgarg123@gmail.com>
2014-12-21 00:27:45 +0530

    updating header

--- application/views/header.php
+++ application/views/header.php
@@ -10,19 +10,19 @@
 	<meta name="description" content="">
 	<meta name="author" content="">
 	
-	<title>Spice Shoppe Stores - Bootstrap 3 Template</title>
+	<title>Vcartx Stores</title>
 	
 	<!-- Bootstrap Core CSS -->
-	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?php echo base_url();?>css/bootstrap.min.css" rel="stylesheet">
 	
 	<!-- Google Web Fonts -->
 	<link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700" rel="stylesheet" type="text/css">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 	
 	<!-- CSS Files -->
-	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
-	<link href="css/style.css" rel="stylesheet">
-	<link href="css/responsive.css" rel="stylesheet">
+	<link href="<?php echo base_url();?>font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<link href="<?php echo base_url();?>css/style.css" rel="stylesheet">
+	<link href="<?php echo base_url();?>css/responsive.css" rel="stylesheet">
 	
 	<!--[if lt IE 9]>
 		<script src="js/ie8-responsive-file-warning.js"></script>
@@ -36,12 +36,102 @@
     <![endif]-->
 	
 	<!-- Fav and touch icons -->
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/fav-144.png">
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/fav-114.png">
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/fav-72.png">
-	<link rel="apple-touch-icon-precomposed" href="images/fav-57.png">
-	<link rel="shortcut icon" href="images/fav.png">
-	
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo base_url();?>images/fav-144.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url();?>images/fav-114.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url();?>images/fav-72.png">
+	<link rel="apple-touch-icon-precomposed" href="<?php echo base_url();?>images/fav-57.png">
+	<link rel="shortcut icon" href="<?php echo base_url();?>images/fav.png">
+	<script>
+	  // // This is called with the results from from FB.getLoginStatus().
+	  // function statusChangeCallback(response) {
+	  //   console.log('statusChangeCallback');
+	  //   console.log(response);
+	  //   // The response object is returned with a status field that lets the
+	  //   // app know the current login status of the person.
+	  //   // Full docs on the response object can be found in the documentation
+	  //   // for FB.getLoginStatus().
+	  //   if (response.status === 'connected') {
+	  //     // Logged into your app and Facebook.	      
+	  //     testAPI();
+	  //   } else if (response.status === 'not_authorized') {
+	  //     // The person is logged into Facebook, but not your app.
+	  //     $(".logout_btn").css('display', 'none`');
+	  //     $(".fb_login_btn").css('display', 'block');
+	  //   } else {
+	  //     // The person is not logged into Facebook, so we're not sure if
+	  //     // they are logged into this app or not.	
+	  //     $(".logout_btn").css('display', 'none');
+	  //     $(".fb_login_btn").css('display', 'block');      
+	  //   }
+	  // }
+
+	  // // This function is called when someone finishes with the Login
+	  // // Button.  See the onlogin handler attached to it in the sample
+	  // // code below.
+	  // function checkLoginState() {
+	  //   FB.getLoginStatus(function(response) {
+	  //     statusChangeCallback(response);
+	  //   });
+	  // }
+
+	  // window.fbAsyncInit = function() {
+		 //  FB.init({
+		 //    appId      : '383109518510005',
+		 //    cookie     : true,  // enable cookies to allow the server to access 
+		 //                        // the session
+		 //    xfbml      : true,  // parse social plugins on this page
+		 //    version    : 'v2.1' // use version 2.1
+		 //  });
+
+		 //  // Now that we've initialized the JavaScript SDK, we call 
+		 //  // FB.getLoginStatus().  This function gets the state of the
+		 //  // person visiting this page and can return one of three states to
+		 //  // the callback you provide.  They can be:
+		 //  //
+		 //  // 1. Logged into your app ('connected')
+		 //  // 2. Logged into Facebook, but not your app ('not_authorized')
+		 //  // 3. Not logged into Facebook and can't tell if they are logged into
+		 //  //    your app or not.
+		 //  //
+		 //  // These three cases are handled in the callback function.
+
+		 //  FB.getLoginStatus(function(response) {
+		 //  	console.log(JSON.stringify(response));
+		 //    statusChangeCallback(response);
+		 //  });		  
+	  // };
+
+	  // // Load the SDK asynchronously
+	  // (function(d, s, id) {
+	  //   var js, fjs = d.getElementsByTagName(s)[0];
+	  //   if (d.getElementById(id)) return;
+	  //   js = d.createElement(s); js.id = id;
+	  //   js.src = "//connect.facebook.net/en_US/sdk.js";
+	  //   fjs.parentNode.insertBefore(js, fjs);
+	  // }(document, 'script', 'facebook-jssdk'));
+
+	  // // Here we run a very simple test of the Graph API after login is
+	  // // successful.  See statusChangeCallback() for when this call is made.
+	  // function testAPI() {
+	  //   FB.api('/me', function(response) {
+	  //     $(".logout_btn").css('display', 'block');
+	  //     $(".fb_login_btn").css('display', 'none');
+	  //   });
+	  // }
+
+	  // function logOut(){
+  	// 	FB.logout(function(response) {
+		 //  	console.log(JSON.stringify(response));
+	  //       // Person is now logged out
+	  //     });
+	  // }
+	</script>	
+	<script src="<?php echo base_url();?>js/jquery-1.11.1.min.js"></script>
+	<script src="<?php echo base_url();?>js/jquery-migrate-1.2.1.min.js"></script>	
+	<script src="<?php echo base_url();?>js/bootstrap.min.js"></script>
+	<script src="<?php echo base_url();?>js/bootstrap-hover-dropdown.min.js"></script>
+    <script src="<?php echo base_url();?>js/jquery.magnific-popup.min.js"></script>
+	<script src="<?php echo base_url();?>js/custom.js"></script>
 </head>
 <body>
 	<!-- Header Section Starts -->
@@ -93,22 +183,22 @@
 											</span>
 										</a>
 									</li>
-									<li>
+									<!-- <li>
 										<a href="register.html">
 											<i class="fa fa-unlock" title="Register"></i>
 											<span class="hidden-sm hidden-xs">
 												Register
 											</span>
 										</a>
-									</li>
-									<li>
-										<a href="login.html">
-											<i class="fa fa-lock" title="Login"></i>
+									</li> -->
+									<!-- <li>
+										<a href="javascript:void(0);">
 											<span class="hidden-sm hidden-xs">
-												Login
+												<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+												</fb:login-button>
 											</span>
 										</a>
-									</li>
+									</li> -->
 								</ul>
 							</div>
 						</div>
@@ -117,33 +207,30 @@
 						<div class="col-sm-4 col-xs-12">
 							<div class="pull-right">
 							<!-- Currency Starts -->
-								<div class="btn-group">
-									<button class="btn btn-link dropdown-toggle" data-toggle="dropdown">
-										Currency
-										<i class="fa fa-caret-down"></i>
-									</button>
-									<ul class="pull-right dropdown-menu">
-										<li><a tabindex="-1" href="#">Pound </a></li>
-										<li><a tabindex="-1" href="#">US Dollar</a></li>
-										<li><a tabindex="-1" href="#">Euro</a></li>
-									</ul>
-								</div>
+
+								<?php if(!$this->session->userdata('isLoggedIn')) {?>
+									<div class="btn-group">									
+										<a class="btn logout_btn" href="<?php echo base_url()?>home/login">
+											Login
+										</a>
+									</div>
+								<?php } ?>
+								<!-- <div class="btn-group fb_login_btn">
+									<fb:login-button class="btn btn-link" scope="public_profile,email" onlogin="checkLoginState();">
+									</fb:login-button>								
+								</div>	 -->							
 							<!-- Currency Ends -->
 							<!-- Languages Starts -->
+							<?php if($this->session->userdata('isLoggedIn')) {?>
 								<div class="btn-group">
-									<button class="btn btn-link dropdown-toggle" data-toggle="dropdown">
-										Language
-										<i class="fa fa-caret-down"></i>
-									</button>
-									<ul class="pull-right dropdown-menu">
-										<li>
-											<a tabindex="-1" href="#">English</a>
-										</li>
-										<li>
-											<a tabindex="-1" href="#">French</a>
-										</li>
-									</ul>
+									<p class="welcome_message btn">Welcome <a href="<?php echo base_url()?>home/userhome"><?php echo $this->session->userdata('firstname')." ".$this->session->userdata('lastname') ?></a></p>
 								</div>
+								<div class="btn-group">									
+									<a class="btn logout_btn" href="<?php echo base_url()?>home/logout">
+										Logout
+									</a>
+								</div>
+							<?php } ?>
 							<!-- Languages Ends -->
 							</div>
 						</div>
@@ -159,7 +246,7 @@
 					<!-- Logo Starts -->
 						<div class="col-md-6">
 							<div id="logo">
-								<a href="index.html"><img src="images/logo.png" title="Spice Shoppe" alt="Spice Shoppe" class="img-responsive" /></a>
+								<a href="<?php echo base_url();?>"><img src="<?php echo base_url();?>images/logo.png" title="Vcartx" alt="Vcartx" class="img-responsive" /></a>
 							</div>
 						</div>
 					<!-- Logo Starts -->
@@ -192,7 +279,7 @@
 											<tr>
 												<td class="text-center">
 													<a href="product.html">
-														<img src="images/product-images/hcart-thumb1.jpg" alt="image" title="image" class="img-thumbnail img-responsive" />
+														<img src="<?php echo base_url();?>images/product-images/hcart-thumb1.jpg" alt="image" title="image" class="img-thumbnail img-responsive" />
 													</a>
 												</td>
 												<td class="text-left">
@@ -211,7 +298,7 @@
 											<tr>
 												<td class="text-center">
 													<a href="product.html">
-														<img src="images/product-images/hcart-thumb2.jpg" alt="image" title="image" class="img-thumbnail img-responsive" />
+														<img src="<?php echo base_url();?>images/product-images/hcart-thumb2.jpg" alt="image" title="image" class="img-thumbnail img-responsive" />
 													</a>
 												</td>
 												<td class="text-left">
